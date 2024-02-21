dadcf63888e55271edf441c3e966de6ba2a106e2
MoniraAzade <monira.azade@gmail.com>
2024-02-22 00:23:38 +0600

    New changes

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,108 +1,155 @@
-const country = "Bangladesh";
-const continent = "Asia";
-let population = 169.4; // for example population = 13 or 130
-isIsland = true;
-let language = "Bangla";
+//JavaScript Fundamentals - Part 1
 
-console.log(country);
-console.log(continent);
-console.log(population);
-console.log(isIsland);
-console.log(language);
+// const country = "Bangladesh";
+// const continent = "Asia";
+// let population = 169.4; // for example population = 13 or 130
+// isIsland = true;
+// let language = "Bangla";
 
-console.log(population / 2);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// console.log(isIsland);
+// console.log(language);
 
-population++;
+// console.log(population / 2);
 
-console.log(population);
-console.log(population > 6);
-console.log(population < 33);
+// population++;
 
-const description1 =
-  country +
-  " is in " +
-  continent +
-  ", and its " +
-  population +
-  " million people speak " +
-  language;
+// console.log(population);
+// console.log(population > 6);
+// console.log(population < 33);
 
-console.log(description1);
+// const description1 =
+//   country +
+//   " is in " +
+//   continent +
+//   ", and its " +
+//   population +
+//   " million people speak " +
+//   language;
 
-const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
-console.log(description);
+// console.log(description1);
 
-if ( population > 33){
-  console.log(`${country}'s population is avobe average`);
-}else{
-  console.log(`${country}'s population is ${ 33 - population} million below average`);
-}
+// const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+// console.log(description);
 
-console.log('9' - '5'); // -> 4
-console.log('19' - '13' + '17'); // -> '617'
-console.log('19' - '13' + 17); // ->  23
-console.log('123' < 57); // -> false
-console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
-
-//Equality Operators: == vs. ===
-
-//Declare a variable numNeighbours based on a prompt input like this:
-// const numNeighbours = prompt('How many neighbour countries does your contry have?');
-
-// const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));    //convert numNeighbours to a number
-
-//                                   //use loose equality == for now).
-// if(numNeighbours === 1){          //Change == to ===
-//   console.log('Only 1 border!');    
-// }else if(numNeighbours > 1){          
-//   console.log( 'More than 1 border');
+// if ( population > 33){
+//   console.log(`${country}'s population is avobe average`);
 // }else{
-//   console.log('No borders');     //this block will be executed when numNeighbours is 0 or any other value).
+//   console.log(`${country}'s population is ${ 33 - population} million below average`);
 // }
 
-/*note: When you use ===, JavaScript checks both value and type.
- If numNeighbours is a string, and you're comparing it with a number using ===,
- the condition will likely evaluate to false even if the content of the string represents the same number.
-*/
+// console.log('9' - '5'); // -> 4
+// console.log('19' - '13' + '17'); // -> '617'
+// console.log('19' - '13' + 17); // ->  23
+// console.log('123' < 57); // -> false
+// console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
 
-// Logical Operators
-/* part of the solution is under below:
-   condition -> language === 'English' && population < 50 && !isIsland  */
+// //Equality Operators: == vs. ===
 
-if(language === 'English' && population < 50 && !isIsland){
-  console.log(`You should live in ${country} :)'`);
-}else{
-  console.log(`${country} does not meet your criteria :('`);
-}
+// //Declare a variable numNeighbours based on a prompt input like this:
+// // const numNeighbours = prompt('How many neighbour countries does your contry have?');
 
-// The switch Statement 
-//It's an alternative to using multiple if-else statements when you need to compare a single value against multiple possible values.
-/*The expression is evaluated once.
--> The value of the expression is compared with the values of each case.
--> If there's a match, the corresponding code block is executed.
--> If no match is found, the code block under the default case (if provided) is executed. The default case is optional.
- */
+// // const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));    //convert numNeighbours to a number
 
-switch (language){
-  case 'chinese': 
-  case 'mandarin':
-       console.log('MOST number of native speakers!');
-       break;
-  case 'spanish':
-       console.log('2nd place in number of native speakers');
-       break;
-  case 'english':
-    console.log('3rd place');
-    break;
-  case 'hindi':
-    console.log('Number 4');
-    break;
-  case 'arabic':
-    console.log('5th most spoken language');
-    break;
-  default:
-    console.log('Great language too :D');
-}
+// //                                   //use loose equality == for now).
+// // if(numNeighbours === 1){          //Change == to ===
+// //   console.log('Only 1 border!');    
+// // }else if(numNeighbours > 1){          
+// //   console.log( 'More than 1 border');
+// // }else{
+// //   console.log('No borders');     //this block will be executed when numNeighbours is 0 or any other value).
+// // }
 
-//The Conditional(Ternary) Operator
-console.log(`${country}'s population is ${population > 33 ? 'above' : 'below'} average`);
+// /*note: When you use ===, JavaScript checks both value and type.
+//  If numNeighbours is a string, and you're comparing it with a number using ===,
+//  the condition will likely evaluate to false even if the content of the string represents the same number.
+// */
+
+// // Logical Operators
+// /* part of the solution is under below:
+//    condition -> language === 'English' && population < 50 && !isIsland  */
+
+// if(language === 'English' && population < 50 && !isIsland){
+//   console.log(`You should live in ${country} :)'`);
+// }else{
+//   console.log(`${country} does not meet your criteria :('`);
+// }
+
+// // The switch Statement 
+// //It's an alternative to using multiple if-else statements when you need to compare a single value against multiple possible values.
+// /*The expression is evaluated once.
+// -> The value of the expression is compared with the values of each case.
+// -> If there's a match, the corresponding code block is executed.
+// -> If no match is found, the code block under the default case (if provided) is executed. The default case is optional.
+//  */
+
+// switch (language){
+//   case 'chinese': 
+//   case 'mandarin':
+//        console.log('MOST number of native speakers!');
+//        break;
+//   case 'spanish':
+//        console.log('2nd place in number of native speakers');
+//        break;
+//   case 'english':
+//     console.log('3rd place');
+//     break;
+//   case 'hindi':
+//     console.log('Number 4');
+//     break;
+//   case 'arabic':
+//     console.log('5th most spoken language');
+//     break;
+//   default:
+//     console.log('Great language too :D');
+// }
+
+// //The Conditional(Ternary) Operator
+// console.log(`${country}'s population is ${population > 33 ? 'above' : 'below'} average`);
+
+
+
+//JavaScript Fundamentals - Part 2
+
+// // # Functions #
+// function describeCountry (country, population, capitalCity){
+//   return `${country} has ${population} million people and it's capital city is ${capitalCity} .`;
+// }
+// const describeBangladesh = describeCountry('Bangladesh', 169.4, 'Dhaka');
+// const describePortugal = describeCountry('Portugual', 10, 'Lisbon');
+// const describeFinland = describeCountry('Finland', 6 , 'Helsinki');
+// console.log(describeBangladesh , describePortugal, describeFinland);
+
+// // #Functiontion Declarations Vs Expressions#
+function percentageOfWorld1 (population){   //use function declarations
+  return (population / 7900) * 100;
+};
+
+// const percentageOfWorld2 = function (population){   //use function expressions
+//   return (population / 7900) * 100;
+// };
+
+// const percentageOfBangladesh1 = percentageOfWorld1(169.4);
+// const percentageOfPortugal1 = percentageOfWorld1(16);
+// const percentageOfChina1 = percentageOfWorld1(1441);
+// console.log(percentageOfBangladesh1, percentageOfPortugal1, percentageOfChina1);
+
+// #Arrow Functions#
+// const percentageOfWorld3 = population => (population / 7900) * 100; //use single line arrow function
+// const percentageOfBangladesh3 = percentageOfWorld3(169.4);
+// const percentageOfPortugal3 = percentageOfWorld3(16);
+// const percentageOfChina3 = percentageOfWorld3(1441);
+
+// console.log(percentageOfBangladesh3 , percentageOfPortugal3, percentageOfChina3);
+
+// #Functions Calling Other Functions#
+const describePopulation = function (country, population){
+    const percentage = percentageOfWorld1 (population);
+    const description = `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+    console.log(description);
+  };
+describePopulation('Bangladesh', 169.4);
+describePopulation('Portugal', 16);
+describePopulation('China', 1441);
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -51,8 +51,92 @@ const age1 = calcAge1(1991);
                                                  return 2037 - birthYear;
                                                 }
                    as we know expression give us a value.                               */
-const calcAge2 = function (birthYear){
-    return 2037 - birthYear;
-}
-const age2 = calcAge2(1991);
-console.log(age1, age2);
+// const calcAge2 = function (birthYear){
+//     return 2037 - birthYear;
+// }
+// const age2 = calcAge2(1991);
+// console.log(age1, age2);
+
+//////////////////////////////////// ///
+// Arrow functions
+ 
+// const calcAge3 = birthYear => 2037 - birthYear; //for single line code
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYear , firstName) => {   //for multiline code and for many parameters
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age ;
+//     // return retirement;
+//     return `${firstName}  retires in ${retirement} years`;
+// }
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1980, 'Bob'));
+
+///////////////////////////////////////
+// Functions Calling Other Functions
+// const cutPieces = function (fruit){
+//     return fruit * 4;
+// }
+
+// function fruitProcessor (apples, oranges){
+//     const applePieces = cutPieces(apples);
+//     const orangePieces = cutPieces(oranges);
+    
+//     const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+//     return juice;
+// };
+// console.log(fruitProcessor(2, 3));
+
+///////////////////////////////////////
+// Reviewing Functions
+/*
+const calcAge = function (birthYeah) {
+    return 2037 - birthYeah;
+  }
+  
+  const yearsUntilRetirement = function (birthYeah, firstName) {
+    const age = calcAge(birthYeah);
+    const retirement = 65 - age;
+  
+    if (retirement > 0) {
+      console.log(`${firstName} retires in ${retirement} years`);
+      return retirement;
+    } else {
+      console.log(`${firstName} has already retired 🎉`);
+      return -1;
+    }
+  }
+  
+  console.log(yearsUntilRetirement(1991, 'Jonas'));
+  console.log(yearsUntilRetirement(1950, 'Mike'));
+*/
+  
+  ///////////////////////////////////////
+  // Coding Challenge #1
+//   const calcAverage = (a, b, c) => (a + b + c) / 3;
+// console.log(calcAverage(3, 4, 5));
+
+// // Test 1
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+// console.log(scoreDolphins, scoreKoalas);
+
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+//   } else {
+//     console.log('No team wins...');
+//   }
+// }
+// checkWinner(scoreDolphins, scoreKoalas);
+
+// checkWinner(576, 111);
+
+// // Test 2
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+// console.log(scoreDolphins, scoreKoalas);
+// checkWinner(scoreDolphins, scoreKoalas);
