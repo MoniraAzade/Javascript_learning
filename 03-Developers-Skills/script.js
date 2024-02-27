f467708bfa572f75919326aa709cf3e93a06b182
MoniraAzade <monira.azade@gmail.com>
2024-02-28 00:53:39 +0600

    28/02/2024

--- 03-Developers-Skills/script.js
+++ 03-Developers-Skills/script.js
@@ -1,10 +1,26 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+// 'use strict';
 
-const x = 23;
-if (x === 23) {
-  console.log(23);
-}
-const calcAge = (birthYear) => 2037 - birthYear;
+// const x = 23;
+// if (x === 23) {
+//   console.log(23);
+// }
+// const calcAge = (birthYear) => 2037 - birthYear;
 
-console.log();
+// console.log();
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celcious",
+    //C) FIX
+    value: Number(prompt("Degree celsius:")),
+  };
+  //B) FIND
+  console.table(measurement);
+  // console.log(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+//A) IDENTIFY
+console.log(measureKelvin());
---
+++ Problem-Solving/StringProblem1.js
@@ -0,0 +1,5 @@
+// Convert and string from "This-is-hello-world" or "This_is_hello_world" to camelCase like this : "thisIsHelloWorld"
+
+const str = 'This-is-hello-world';
+
+//output will be like : "thisIsHelloWorld"
